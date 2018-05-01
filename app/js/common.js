b773411d229dd02601614e4ce297c1c1dbf0e6fa
Pavel Bezdornov <pavel.alpinweb@yandex.ru>
2018-05-01 17:21:33 +0600

    change styles for scripts section comand

--- app/js/common.js
+++ app/js/common.js
@@ -1,2 +1,4 @@
-$(document).ready(function(){$("form").submit(function(){var th=$(this);$.ajax({type:"POST",url:"mail.php",data:th.serialize()}).done(function(){alert("Thank you!");setTimeout(function(){th.trigger("reset");},1000);});return false;});});
+$(document).ready(function(){
+
+});
 
