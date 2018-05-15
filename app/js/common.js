4804602afb03e33af7755a145b5a60339594c82c
Pavel Bezdornov <pavel.alpinweb@yandex.ru>
2018-05-15 20:16:43 +0600

    full page menu

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
-
+    $('.btn_mnu').click(function (e) { 
+        $('.nav').fadeIn();
+    });
+    $('.close-menu').click(function (e) { 
+        $('.nav').fadeOut(); 
+    });
 });
 
